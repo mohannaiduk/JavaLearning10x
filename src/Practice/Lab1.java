06ef6ae5165d8dcc695f2fb52a7554324566872d
mohan <mohanmayudukmn@gmail.com>
2025-01-11 21:31:59 +0530

    commit

--- src/Practice/Lab1.java
+++ src/Practice/Lab1.java
@@ -1,2 +1,14 @@
-package Practice;public class Lab1 {
+package Practice;
+
+public class Lab1 {
+    public static void main(String[] args) {
+            int b;
+        for (int i = 1; i <=10; i++) {
+
+            b=9*i;
+            System.out.printf("9*%d=%d", i,b);
+            System.out.println();
+
+        }
+    }
 }
